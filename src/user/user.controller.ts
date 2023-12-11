c694be55823efdbf2b0ab700031bfd0df4952d22
ultr4 <cl.rch09@icloud.com>
2023-12-11 16:33:48 -0300

    :sparkles: feat: Added Create route

---
+++ src/user/user.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('create')
+  async createUser(
+    @Body() CreateUserDto: CreateUserDto,
+  ): Promise<User | object> {
+    return await this.userService.createUser(CreateUserDto);
+  }
+}
