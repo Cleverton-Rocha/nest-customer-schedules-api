1d48c5a367a3cf5eb4aa4424ec519bab1e8d6cdb
ultr4 <cl.rch09@icloud.com>
2023-12-11 16:32:59 -0300

    :sparkles: feat: Configured Sqlite connection

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sql',
+      synchronize: true,
+      entities: [User],
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
