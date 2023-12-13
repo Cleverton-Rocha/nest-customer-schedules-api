039313bd27f0c177e7ffada5c1c5f98350233d50
ultr4 <cl.rch09@icloud.com>
2023-12-13 11:35:19 -0300

    :sparkles: feat: Made "Get" routes public

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -11,6 +11,7 @@ import { SchedulesService } from './schedules.service';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
 import { Schedule } from './entities/schedule.entity';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('schedules')
 export class SchedulesController {
@@ -21,21 +22,25 @@ export class SchedulesController {
     return await this.schedulesService.createSchedule(createScheduleDto);
   }
 
+  @IsPublic()
   @Get('all')
   async getSchedule(): Promise<Schedule[]> {
     return await this.schedulesService.findAll();
   }
 
+  @IsPublic()
   @Get('id/:id')
   async getOneSchedule(@Param('id') id: number): Promise<Schedule> {
     return await this.schedulesService.findOne(id);
   }
 
+  @IsPublic()
   @Get('day/:day')
   async getByDay(@Param('day') day: string): Promise<Schedule> {
     return await this.schedulesService.findByDay(day);
   }
 
+  @IsPublic()
   @Get('hour/:hour')
   async getByHour(@Param('hour') hour: string): Promise<Schedule> {
     return await this.schedulesService.findByHour(hour);
