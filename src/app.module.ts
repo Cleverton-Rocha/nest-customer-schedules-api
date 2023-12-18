f2e367fcc1259d3f7ddf33bb3eba303de38ea0e7
ultr4 <cl.rch09@icloud.com>
2023-12-18 06:42:27 -0300

    :sparkles: feat: Added postgreSQL db connection params

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,14 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db/sql',
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT) || 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       synchronize: true,
+      ssl: true,
       entities: [User, Schedule],
     }),
     UserModule,
